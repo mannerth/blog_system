2ebab9cab4a616250386c8f2186c1932e22260ff
mannerth <3502281960@qq.com>
2026-04-21 22:11:51 +0800

    init: 前后端模版代码

---
+++ blog_system_backend/src/main/java/com/example/blog_system_backend/BlogSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSystemBackendApplication.class, args);
+    }
+
+}
---
+++ blog_system_backend/src/test/java/com/example/blog_system_backend/BlogSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
